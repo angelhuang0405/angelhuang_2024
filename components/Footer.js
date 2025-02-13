905a233f9ded0c6645224ebd03e7156da006c7bc
Ken Huang <bluetch@gmail.com>
2025-02-13 08:04:19 +0800

    component: footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,31 +1,31 @@
 import Link from "next/link";
 import { Icon } from "components";
 
-const links = [
-  {
-    name: "facebook",
-    url: "https://www.facebook.com/bluetch",
-  },
-  {
-    name: "linkedin",
-    url: "https://www.linkedin.com/in/bluetch/",
-  },
-  {
-    name: "github",
-    url: "https://github.com/bluetch",
-  },
-  {
-    name: "medium",
-    url: "https://medium.com/@bluetch",
-  },
-  {
-    name: "instagram",
-    url: "https://www.instagram.com/kenbluerr/",
-  },
-];
-
 export const Footer = () => {
   // Footer component
+  const links = useMemo(() => [
+    {
+      name: "facebook",
+      url: "https://www.facebook.com/bluetch",
+    },
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/bluetch/",
+    },
+    {
+      name: "github",
+      url: "https://github.com/bluetch",
+    },
+    {
+      name: "medium",
+      url: "https://medium.com/@bluetch",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/kenbluerr/",
+    },
+  ], []);
+  
   return (
     <footer className="text-sm text-gray-500 text-center lg:py-20 py-8 space-y-8">
       <p className="lg:text-xl text-black">Feel free to reach out, I’d love to chat with you!</p>
